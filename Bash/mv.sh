ad66ae93355ca5a12a04ecb633eab3f56ee15efd
Oleg Tarasevich <tarasevich.oleg@gmail.com>
2017-09-13 23:46:26 +0300

    Add comment

--- Bash/mv.sh
+++ Bash/mv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+#Перемещение с проверкой существования файла
 if [[ ! $(find /SearchDirectory/ -name 'Namefile*' | wc -l) = 0 ]]; then
 mv $(find /SearchDirectory/ -name 'Namefile*') /DestinationDirectory/ ;
 else echo "No Namefile*" ; fi
